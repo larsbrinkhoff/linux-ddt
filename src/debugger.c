b423e595b9c7c83483f59156e69de3a466b12be6
Kevin Johnson <aloof.schipperke@gmail.com>
2018-11-06 13:02:52 +0000

    added step_job() to debugger.c

--- src/debugger.c
+++ src/debugger.c
@@ -1,7 +1,9 @@
-#include <sys/reg.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <stdio.h>
 #include <termios.h>
 #include <sys/ptrace.h>
+#include <sys/reg.h>
 #include "files.h"
 #include "jobs.h"
 
@@ -11,4 +13,12 @@ void typeout_pc(struct job *j)
   long long data = ptrace(PTRACE_PEEKDATA, j->proc.pid, pc, NULL);
 
   fprintf(stderr, "%llx)   %llx   ", pc, data);
+}
+
+void step_job(struct job *j)
+{
+  int status;
+  if (ptrace(PTRACE_SINGLESTEP, j->proc.pid, NULL, NULL) == -1)
+    errout("ptrace");
+  check_jobs();
 }
--- src/debugger.h
+++ src/debugger.h
@@ -1,1 +1,2 @@
 void typeout_pc(struct job *j);
+void step_job(struct job *j);
