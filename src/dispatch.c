c345ae5cbb16aba46eca74c09ed54a8b4c74fe8d
Kevin Johnson <aloof.schipperke@gmail.com>
2018-11-19 21:31:18 +0000

    added the '=' ddt command to test expression evaluation

--- src/dispatch.c
+++ src/dispatch.c
@@ -8,6 +8,7 @@
 #include "jobs.h"
 #include "user.h"
 #include "debugger.h"
+#include "aeval.h"
 
 #define PREFIX_MAXBUF 255
 #define SUFFIX_MAXBUF 255
@@ -374,6 +375,33 @@ void files (void)
   done = 1;
 }
 
+void showq (void)
+{
+  if (nprefix)
+    {
+      uint64_t n = 0;
+      char *r;
+      if ((r = evalexpr(prefix, &n))
+	  && !*r)
+	qreg = n;
+      else
+	{
+	  fputs("?? ", stderr);
+	  goto leave;
+	}
+    }
+  if (altmodes)
+    {
+      fprintf(stderr, "%f   ", (double)qreg);
+      altmodes = 0;
+    }
+  else
+    fprintf(stderr, "%lu   ", qreg);
+
+ leave:
+  prefix[nprefix] = nprefix = 0;
+}
+
 static void chquote (void)
 {
   character = term_read();
@@ -469,6 +497,9 @@ void dispatch_init (void)
 
   plain[':'] = colon;
   alt[':'] = colon;
+  plain['='] = showq;
+  alt['='] = showq;
+
   alt['g'] = start;
   alt['j'] = job;
   alt['l'] = load;
